06c80c3baa5f20111494acd3b5f837fb2a2c3aa4
qhe132 <qhe132@users.noreply.github.com>
2017-11-14 21:47:28 +0800

    second

--- ipet/src/pages/map/map.ts
+++ ipet/src/pages/map/map.ts
@@ -20,20 +20,137 @@ export class MapPage {
   ionViewDidLoad() {
     let map = new AMap.Map('container',{
       resizeEnable: true,
+      rotateEnable: true,
+      pitchEnable: true,
+      pitch:60,
+      rotation:-10,
       zoom : 15,
+      viewMode:'3D',
+      buildingAnimation:true,
+      expandZoomRange:true,
+      zooms:[3,15],
       center:[120.388381,30.3086194]
     }) ;
-    AMap.plugin(['AMap.ToolBar'],function(){
-      map.addControl(new AMap.ToolBar());
+    let marker = new AMap.Marker({
+      icon:"http://webapi.amap.com/theme/v1.3/markers/n/mark_b.png",
+      position:map.getCenter(),
     });
 
-
-    let marker = new AMap.Marker({
-      position : map.getCenter(),
-      draggable: true,
-      cursor: 'move'
-    })
     marker.setMap(map);
-    marker.setAnimation('AMAP_ANIMATION_BOUNCE');
+    /*AMap.plugin(['AMap.ToolBar'],function(){
+      map.addControl(new AMap.ToolBar());
+    });*/
+    AMap.plugin(['AMap.ControlBar'],function(){
+      map.addControl(new AMap.ControlBar({
+        showZoomBar:false,
+        showControlButton:true,
+        position:{
+          right:'10px',
+          top:'10px'
+        }
+      }));
+    });
+    /*marker.setMap(map);
+    marker.setAnimation('AMAP_ANIMATION_BOUNCE');*/
+  }
+  ourposition(){
+    let map = new AMap.Map('container',{
+      resizeEnable: true,
+      rotateEnable: true,
+      pitchEnable: true,
+      pitch:60,
+      rotation:-10,
+      zoom : 15,
+      viewMode:'3D',
+      buildingAnimation:true,
+      expandZoomRange:true,
+      zooms:[3,15],
+      center:[120.388381,30.3086194]
+    }) ; 
+    let marker = new AMap.Marker({
+      icon:"http://webapi.amap.com/theme/v1.3/markers/n/mark_b.png",
+      position:map.getCenter(),
+    });
+    marker.setMap(map);
+    AMap.plugin(['AMap.ControlBar'],function(){
+      map.addControl(new AMap.ControlBar({
+        showZoomBar:false,
+        showControlButton:true,
+        position:{
+          right:'10px',
+          top:'10px'
+        }
+      }));
+    });
+  }
+  petposition(){
+    let map = new AMap.Map('container',{
+      resizeEnable: true,
+      rotateEnable: true,
+      pitchEnable: true,
+      pitch:60,
+      rotation:-10,
+      zoom : 15,
+      viewMode:'3D',
+      buildingAnimation:true,
+      expandZoomRange:true,
+      zooms:[3,15],
+      center:[120.388381,30.3386194]
+    }) ; 
+    let marker = new AMap.Marker({
+      icon:"http://webapi.amap.com/theme/v1.3/markers/n/mark_b.png",
+      position:map.getCenter(),
+    });
+    marker.setMap(map);
+    AMap.plugin(['AMap.ControlBar'],function(){
+      map.addControl(new AMap.ControlBar({
+        showZoomBar:false,
+        showControlButton:true,
+        position:{
+          right:'10px',
+          top:'10px'
+        }
+      }));
+    }); 
+  }
+  navto(){
+    let map = new AMap.Map('container',{
+      resizeEnable: true,
+      rotateEnable: true,
+      pitchEnable: true,
+      pitch:60,
+      rotation:-10,
+      zoom : 15,
+      viewMode:'3D',
+      buildingAnimation:true,
+      expandZoomRange:true,
+      zooms:[3,15],
+      center:[120.388381,30.3086194]
+    }) ;
+    let marker = new AMap.Marker({
+      icon:"http://webapi.amap.com/theme/v1.3/markers/n/mark_b.png",
+      position:map.getCenter(),
+    });
+
+    let walking = new AMap.Walking({
+      map:map,
+      panel:""
+    });
+
+    walking.search([120.388381,30.3086194],[120.388381,30.3386194]);
+    marker.setMap(map);
+    /*AMap.plugin(['AMap.ToolBar'],function(){
+      map.addControl(new AMap.ToolBar());
+    });*/
+    AMap.plugin(['AMap.ControlBar'],function(){
+      map.addControl(new AMap.ControlBar({
+        showZoomBar:false,
+        showControlButton:true,
+        position:{
+          right:'10px',
+          top:'10px'
+        }
+      }));
+    }) ;
   }
 }
